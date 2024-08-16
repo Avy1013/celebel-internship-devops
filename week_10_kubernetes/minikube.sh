618d601216389ea3239a642583745735afc66b07
Avy1013 <vaibhavgupta.avy1013@gmail.com>
2024-08-16 10:55:12 +0530

    minikube cluster present

---
+++ week_10_kubernetes/minikube.sh
@@ -0,0 +1,10 @@
+
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-latest.x86_64.rpm
+sudo rpm -Uvh minikube-latest.x86_64.rpm
+
+minikube start 
+
+
+# for mac os
+# brew install minikube
